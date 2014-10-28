2ba6cad4cd8b4583532e9a28f7f563b67ce6199f
clynchga <clarevlynch@gmail.com>
2014-10-27 21:35:39 -0400

    adding db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+stores = Store.create([{location: "Austin"}, {location: "Boston"}, {location: "Chicago"}, {location: "Denver"}])
+Product.create(title: "Heffalump", description: "A very frightening purchase indeed", price: "56.70", store_id: stores.first.id)
+Product.create(title: "burrito", description: "Mmmmmmm good", price: 6.59, store_id: stores.first.id)
