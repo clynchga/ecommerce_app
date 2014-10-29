fa451864a802afb90aba6f35198e577e2203198f
clynchga <clarevlynch@gmail.com>
2014-10-28 21:46:41 -0400

    minor fix for product price display

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	belongs_to :store
 	validates :title, presence: true
-	validates :description, presence: true
-	validates :price, presence: true
+	validates :description, length: {minimum: 10}
+	validates :price, numericality: {greater_than: 0}
 end
